2e6c36ee1bd0a866e3f8a1b52b0aa1fc814cbe45
ZeyronJ <93349366+ZeyronJ@users.noreply.github.com>
2024-05-18 18:34:08 -0400

    detalles y cv update

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,15 +1,16 @@
 import ImageGallery from 'react-image-gallery';
 import 'react-image-gallery/styles/css/image-gallery.css';
+import { CarbonCloseOutline } from '../helpers/icons';
 
 function Gallery({ images, handleClose }) {
   return (
     <div className='fixed top-0 left-0 w-full h-full flex justify-center items-center z-50 bg-black bg-opacity-60'>
       <div className='p-6 lg:max-w-4xl max-w-sm'>
         <button
-          className='absolute top-4 right-4 text-white hover:text-gray-200 lg:text-4xl text-2xl underline underline-offset-4'
+          className='absolute top-4 right-4 text-white hover:text-gray-200'
           onClick={handleClose}
         >
-          x
+          <CarbonCloseOutline className='h-8 w-8 lg:w-12 lg:h-12' />
         </button>
         <ImageGallery items={images} />
       </div>
--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import { CarbonCloseOutline } from '../helpers/icons';
 
 function Video({ videoUrl, handleClose }) {
   return (
     <div className='fixed top-0 left-0 w-full h-full flex justify-center items-center z-50 bg-black bg-opacity-60'>
       <div className='p-6 lg:max-w-4xl max-w-sm'>
         <button
-          className='absolute top-4 right-4 text-white hover:text-gray-200 lg:text-4xl text-2xl underline underline-offset-4'
+          className='absolute top-4 right-4 text-white hover:text-gray-200'
           onClick={handleClose}
         >
-          x
+          <CarbonCloseOutline className='h-8 w-8 lg:w-12 lg:h-12' />
         </button>
         <video controls className='h-[36rem]' autoPlay>
           <source src={videoUrl} type='video/mp4' />
